b462173f581b13e0702ef90f0a854f7c0a136c41
Vijay6868 <saroyav3@gmail.com>
2024-02-10 16:44:22 +1300

    fetch data from API, display on web browser, used parameter

--- 03_02_request/begin/script.js
+++ 03_02_request/begin/script.js
@@ -1,9 +1,31 @@
-fetch('https://hplussport.com/api/products')
+fetch('https://hplussport.com/api/products?order=name')
 .then(
     function(response){
         return response.json()
     }
 )
+//.then(function(jsonData){
+    //console.log(jsonData);
+    //var description = jsonData[0].description
+    //console.log(description);
+    //var name = jsonData[0].name
+    //console.log(name)
+
+    //var product = document.createElement("li")
+    //product.innerHTML = name;
+    //document.body.appendChild(product)
+//})
+
 .then(function(jsonData){
-    console.log(jsonData);
+    for(items in jsonData){
+        var productName = jsonData[items].name;
+        var products = document.createElement("li");
+        products.innerHTML=productName;
+        document.body.appendChild(products);
+
+        var producImg = jsonData[items].image;
+        var image = document.createElement("img");
+        image.setAttribute('src',producImg)
+        document.body.appendChild(image);
+    }
 })
