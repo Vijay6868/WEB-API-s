70be657fd192515c5c3f10efed6aa43b5158b7b6
Vijay6868 <saroyav3@gmail.com>
2024-02-11 07:41:26 +1300

    challenge completed

---
+++ 03_07_hplusSportChallenge/script.js
@@ -0,0 +1,26 @@
+fetch('https://hplussport.com/api/products?order=price')
+.then(function(response) {
+	return response.json();
+})
+.then(function(jsonData) {
+	for(items in jsonData) {
+		//Display all the product names
+		var productName = jsonData[items].name;
+		var product = document.createElement('li');
+		product.innerHTML = productName;
+		document.body.appendChild(product);
+
+		//Display all the product images
+		var productImg = jsonData[items].image;
+		var image = document.createElement('img');
+		image.setAttribute('src', productImg);
+		document.body.appendChild(image);
+		
+		//Display the price of each product
+		var productPrice = jsonData[items].price;
+		var price = document.createElement("li");
+		price.innerHTML = productPrice;
+		document.body.appendChild(price);
+	}
+
+})
